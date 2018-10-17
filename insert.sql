68084be97a991b164d37755832da8f3126fecb0d
Steven Rosa <steven.salvatore.rosa@gmail.com>
2018-10-17 15:35:21 -0400

    Lab complete.

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Mercury', 'gray', 0, 0.55, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Venus', 'yellow', 0, 0.82, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Earth', 'blue', 1, 1.00, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Mars', 'red', 2, 0.11, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Jupiter', 'orange', 53, 317.90, 0);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Saturn', 'hazel', 62, 95.19, 1);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Uranus', 'light blue', 27, 14.54, 1);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Neptune', 'dark blue', 14, 17.15, 1);
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ('Pluto', 'brown', 5, 0.003, 0);
---
+++ update.sql
@@ -0,0 +1,1 @@
+UPDATE planets SET num_of_moons = 68 WHERE num_of_moons = 53;
